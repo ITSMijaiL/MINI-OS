f5a37f1d3eea0a5aaffb0c731ea5e2467fa918fd
ITSMijaiL <mijailct@gmail.com>
2021-08-06 09:08:03 -0400

    Alpha Build #5

--- disk/kernel/kmain.lua
+++ disk/kernel/kmain.lua
@@ -1,10 +1,14 @@
 local Kernel={}
 
 local pm = require("processmanager")
+local sm= require("syscallmanager")
+
 
 Kernel.pmanager = pm.ProcessManager:new()
 
+Kernel.syscall = sm.DoCall
+
 function kmain(...)
   local args = {...}
-  --Kernel.pmanager.
+  Kernel.pmanager:addproc(Process:new(nil, 1, Kernel.pmanager,function() os.run("") end)) 
 end 
