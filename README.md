255c0947f4e790e507d3a35736761141d0dc4c51
ITSMijaiL <mijailct@gmail.com>
2021-08-07 23:48:56 -0400

    Alpha Build #8.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MINI-OS
 (W.I.P.) Operative system for computercraft, an incredible mod for minecraft!
+
+***This code is unstable as it is still on the alpha stage, you've been warned.***
--- install.lua
+++ install.lua
@@ -1,1 +1,15 @@
---TODO: make installation
+print("MINI-OS installer")
+print("[V0.0.1]")
+
+print("Are you sure that you want to install this unstable system?")
+write("Y/N:")
+local yn = io.read()
+
+while string.lower(yn)~="y" and string.lower(yn)~="n" and string.lower(yn)~="no" and string.lower(yn)~="yes" do
+    write("Y/N:")
+    yn = io.read()
+end
+
+if string.lower(yn)=="yes" or string.lower(yn)=="y" then
+    --TODO:make directories along with kernel, insert startup.lua, insert libs and insert services to be initiated in (disk/rootfs)/etc/on_init/
+end
