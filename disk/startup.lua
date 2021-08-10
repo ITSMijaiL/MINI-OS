7d597eef8e101fa9052f200ded92a3e6cbf71839
ITSMijaiL <mijailct@gmail.com>
2021-08-10 13:15:11 -0400

    Alpha Build #9.5.4

--- disk/startup.lua
+++ disk/startup.lua
@@ -5,9 +5,7 @@ print("INITIALIZING KERNEL...")
 local err,msg = nil,nil
 parallel.waitForAny(function() err,msg = pcall(loadfile("/disk/kernel/kmain.lua")) end)
 
-if msg~= nil then
-    assert(err,"[KERNEL PANIC!] Kernel's object couldn't be initialized due to an error!\n"..msg)
-end
+if not err then printError("[KERNEL PANIC!] Kernel's object couldn't be initialized due to an error!\n"..msg) end
 
 assert(_G.Kernel ~= nil,"[KERNEL PANIC!] Kernel's object is non-existent!")
 
